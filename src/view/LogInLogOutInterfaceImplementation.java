d07a833d4a3d7be18e22c6f710590b9d2352a225
2dam <2dam@HZ375757.TartangaLH.eus>
2022-10-24 09:28:23 +0200

    implementation started

---
+++ src/view/LogInLogOutInterfaceImplementation.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import Classes.Login_Logout;
+import Classes.User;
+
+/**
+ *
+ * @author 2dam
+ */
+public class LogInLogOutInterfaceImplementation implements Login_Logout {
+
+    @Override
+    public User login(User user) {
+        
+        
+        return user;
+    }
+
+    @Override
+    public User SignIn(User user) {
+        
+        
+        
+        
+        return user;
+    }
+    
+}
