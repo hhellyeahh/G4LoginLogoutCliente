92220d4479be89a98b1685719e82c01046a0ece5
2dam <2dam@HZ375757.TartangaLH.eus>
2022-11-02 14:24:03 +0100

    conflicts resolved

--- src/view/LogIn/LogInController.java
+++ src/view/LogIn/LogInController.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -192,6 +191,9 @@ public class LogInController implements Initializable {
         controller.initData(loginUser);
 
         controller.initialize(root);
+        
+        tfUsername.setText("");
+        pfPassword.setText("");
     
     }
     protected void showErrorAlert(String errorMsg) {
@@ -202,4 +204,3 @@ public class LogInController implements Initializable {
         alert.showAndWait();
     }
 }
-
