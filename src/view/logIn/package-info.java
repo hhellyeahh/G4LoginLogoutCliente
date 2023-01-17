e617c453e84b549cf0ad283b1963b6469eefbae5
2dam <2dam@HZ375749.TartangaLH.eus>
2023-01-17 14:10:52 +0100

    *

--- src/view/logIn/package-info.java
+++ src/view/logIn/package-info.java
@@ -3,4 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
+package view.logIn;
+
+/**
+ * This folder contains the login view
+ */
